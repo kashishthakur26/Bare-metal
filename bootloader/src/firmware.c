fbdba90b86592cb64a537b89571aacdf1924d0e5
kashishthakur26 <kashishthakur.official.0907@gmail.com>
2026-02-23 06:53:02 +0530

    Bootloader

---
+++ bootloader/src/firmware.c
@@ -0,0 +1,24 @@
+#include "common-defines.h"
+#include <libopencm3/stm32/memorymap.h>
+
+#define BOOTLOADER_SIZE      (0x8000U)
+#define MAIN_APP_START_ADDRESS (FLASH_BASE + BOOTLOADER_SIZE)
+
+void jump_to_main(void){
+
+    typedef void (*void_fn)(void);
+    uint32_t* reset_vector_entry = (uint32_t*)MAIN_APP_START_ADDRESS + 4U;
+    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
+
+    void_fn  jump_fn = (void)(reset_vector);
+
+}
+
+int main(void){
+
+    jump_to_main();
+
+
+
+    return 0;
+}
